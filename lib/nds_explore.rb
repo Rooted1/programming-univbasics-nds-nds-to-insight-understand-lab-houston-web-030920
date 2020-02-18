1a396c229a458214d19d5cba442c47d608ea4fe1
Ruth Obe <ruthobey@gmail.com>
2020-02-18 20:48:25 +0000

    Automatically backed up by Learn

---
+++ lib/nds_explore.rb
@@ -0,0 +1,12 @@
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+require 'directors_database'
+require 'pry'
+require 'pp'
+
+def pretty_print_nds(nds)
+  pp nds
+end
+
+def print_first_directors_movie_titles
+
+end
