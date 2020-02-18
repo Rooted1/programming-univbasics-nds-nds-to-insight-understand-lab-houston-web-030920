2e75469ef8f1526c41b850cabe42795262f6ad34
Ruth Obe <ruthobey@gmail.com>
2020-02-18 21:01:09 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -1,14 +1,16 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 require 'pry'
-require 'pp'
+# require 'pp'
 
 def pretty_print_nds(nds)
   pp nds
 end
 
 def print_first_directors_movie_titles
+  
   binding.pry 
+  
   all_movies = directors_database[0][:movies]
   
   index = 0
